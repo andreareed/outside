7290c09a38fda6fc3cb07f983e4ecb0ac8f484b0
Andrea Reed <andrea@chasingatoms.com>
2023-05-02 17:44:23 -0400

    create custom editor script for ItemSO

--- Assets/Scripts/Inventory/ItemSO.cs
+++ Assets/Scripts/Inventory/ItemSO.cs
@@ -1,29 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Game/Inventory/New Item")]
 public class ItemSO : ScriptableObject
 {
+  // This SO uses a customer editor to display the fields in a more organized way.
+  // Always update the editor script when making changes
+
   public enum _ItemType { Generic, Consumable, Weapon }
 
-  [Header("General")]
+  // UI Display
   [SerializeField] _ItemType itemType;
   [SerializeField] Sprite icon;
   [SerializeField] string itemName;
   [SerializeField] string description;
   [Space]
-  [Header("Item Stacking")]
+
+  // Stacking
   [SerializeField] bool isStackable = false;
   [SerializeField] int maxStack = 1;
 
-  [Header("Consumable")]
+  // Consumable
   [SerializeField] float health = 10f;
   [SerializeField] float food = 10f;
   [SerializeField] float water = 10f;
 
-  [Header("Weapon")]
+  // Weapon
   [SerializeField] float damage = 10f;
   [SerializeField] float range = 10f;
   [SerializeField] float fireRate = 1f;
@@ -48,3 +55,75 @@ public class ItemSO : ScriptableObject
   public float ReloadTime => reloadTime;
   public int MaxAmmo => maxAmmo;
 }
+
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(ItemSO))]
+public class ItemSOEditor : Editor
+{
+
+  public override void OnInspectorGUI()
+  {
+    EditorGUILayout.Space();
+    GUILayout.Label("Inventory Item", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold, fontSize = 18 }, GUILayout.ExpandWidth(true));
+    EditorGUILayout.Space();
+    EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+    EditorGUILayout.Space();
+
+    GUILayout.Label("UI Display", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleLeft, fontStyle = FontStyle.Bold, fontSize = 14 }, GUILayout.ExpandWidth(true));
+    EditorGUILayout.Space();
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("icon"));
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("itemName"));
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("description"));
+    EditorGUILayout.Space();
+    EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+    EditorGUILayout.Space();
+
+    GUILayout.Label("Stacking", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleLeft, fontStyle = FontStyle.Bold, fontSize = 14 }, GUILayout.ExpandWidth(true));
+    EditorGUILayout.Space();
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("isStackable"));
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("maxStack"));
+    EditorGUILayout.Space();
+    EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+    EditorGUILayout.Space();
+
+    GUILayout.Label("Item Effects", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleLeft, fontStyle = FontStyle.Bold, fontSize = 14 }, GUILayout.ExpandWidth(true));
+    EditorGUILayout.Space();
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("itemType"));
+    EditorGUILayout.Space();
+
+    int selectedItemType = serializedObject.FindProperty("itemType").intValue;
+
+    switch (selectedItemType)
+    {
+      case 1:
+        DisplayConsumableInfo();
+        break;
+
+      case 2:
+        DisplayWeaponInfo();
+        break;
+
+    }
+    serializedObject.ApplyModifiedProperties();
+  }
+
+  void DisplayConsumableInfo()
+  {
+
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("health"));
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("food"));
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("water"));
+  }
+
+  void DisplayWeaponInfo()
+  {
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("damage"));
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("range"));
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("fireRate"));
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("reloadTime"));
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("maxAmmo"));
+  }
+}
+
+#endif
